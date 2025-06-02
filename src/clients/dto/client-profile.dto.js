c7ec5daef60da4aa42d12e6fb110362d66750c76
Jagan Kumar Hotta <138506773+techySPHINX@users.noreply.github.com>
2025-06-02 23:22:51 +0530

    remove : build files for production

--- src/clients/dto/client-profile.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ClientProfileDto = void 0;
-class ClientProfileDto {
-}
-exports.ClientProfileDto = ClientProfileDto;
